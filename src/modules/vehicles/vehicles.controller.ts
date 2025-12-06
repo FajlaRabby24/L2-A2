747a864089aa8f2104dd60ac3a1d86bc8596978a
FajlaRabby24 <fajlarabbyla24@gmail.com>
2025-12-06 13:14:37 +0600

    create vehicles controller and service created

---
+++ src/modules/vehicles/vehicles.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { sendResponse } from "../../utils/sendResponse";
+import { vechclesService } from "./vehicles.service";
+
+const createVehicles = async (req: Request, res: Response) => {
+  try {
+    const result = await vechclesService.createVehicles(req.body);
+
+    sendResponse(res, 200, true, "Vehicles created successfully", result);
+  } catch (error: any) {
+    sendResponse(res, 500, false, error.message);
+  }
+};
+
+export const vechclesController = {
+  createVehicles,
+};
--- src/modules/vehicles/vehicles.route.ts
+++ src/modules/vehicles/vehicles.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { vechclesController } from "./vehicles.controller";
 
 const router = Router();
+
+router.post("/", vechclesController.createVehicles);
 
 export const vehiclesRoutes = router;
